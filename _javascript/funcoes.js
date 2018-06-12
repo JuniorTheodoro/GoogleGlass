ea13bc5863d643ad1863e817669db0a042ea8f53
JuniorTheodoro <juninho_lpisrock@hotmail.com>
2018-06-12 20:22:38 -0300

    finalização fale-conosco

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,3 +1,8 @@
 function trocar(imagem) {
 	document.getElementById('icone').src = imagem;
 }
+function calculaTotal(){
+	var qtd = parseInt(document.getElementById('campoQtd').value);
+	total = qtd * 1500;
+	document.getElementById('campoPreco').value = total;
+}
